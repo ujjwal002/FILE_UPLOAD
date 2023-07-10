3c317c8a7b68d870aacd501a9ec691d34374c5b9
ujjwal kumar <79785180+ujjwal002@users.noreply.github.com>
2023-07-10 11:23:05 +0530

    Update FileUpload.js
    Added a routed to add to video files

--- routes/FileUpload.js
+++ routes/FileUpload.js
@@ -8,5 +8,6 @@ const {localFileUpload, imageUpload} = require('../controllers/fileUpload');
 
 router.post('/localFileUpload',localFileUpload);
 router.post('/imageUpload',imageUpload);
+router.post('/videoUpload',imageUpload);
 
 module.exports = router;
