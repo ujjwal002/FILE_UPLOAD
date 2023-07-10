092b2128b0c5bf809942fb486eb7b17bd39ee56e
ujjwal kumar <79785180+ujjwal002@users.noreply.github.com>
2023-07-10 11:36:40 +0530

    Update FileUpload.js
    add a routes for image size reducer for code

--- routes/FileUpload.js
+++ routes/FileUpload.js
@@ -3,11 +3,12 @@ const express  = require('express');
 const router = express.Router();
 
 
-const {localFileUpload, imageUpload,videoUpload} = require('../controllers/fileUpload');
+const {localFileUpload, imageUpload,videoUpload,imageSizeReducer} = require('../controllers/fileUpload');
 
 
 router.post('/localFileUpload',localFileUpload);
 router.post('/imageUpload',imageUpload);
-router.post('/videoUpload',imageUpload);
+router.post('/videoUpload',videoUpload);
+router.post('/imageSizeReducer',imageSizeReducer);
 
 module.exports = router;
